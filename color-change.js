a5c9aeec73ded24441aed3203858d8ce7821bf29
RinaOcean <antyfifa@gmail.com>
2021-02-27 01:57:33 +0200

    Update color-change.js

--- color-change.js
+++ color-change.js
@@ -1,21 +1,21 @@
 const colors = [
-  "#FFFFFF",
-  "#2196F3",
-  "#4CAF50",
-  "#FF9800",
-  "#009688",
-  "#795548",
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
 ];
 
-const bodyRef = document.querySelector("body");
+const bodyRef = document.querySelector('body');
 const startRef = document.querySelector('[data-action="start"]');
 const stopRef = document.querySelector('[data-action="stop"]');
 
 let intervalId = null;
 
-startRef.addEventListener("click", startColorChange);
+startRef.addEventListener('click', startColorChange);
 
-stopRef.addEventListener("click", stopColorChange);
+stopRef.addEventListener('click', stopColorChange);
 
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
@@ -26,10 +26,10 @@ function startColorChange() {
     bodyRef.style.backgroundColor =
       colors[randomIntegerFromInterval(0, colors.length - 1)];
   }, 1000);
-  startRef.removeEventListener("click", startColorChange);
+  startRef.removeEventListener('click', startColorChange);
 }
 
 function stopColorChange() {
   clearInterval(intervalId);
-  startRef.addEventListener("click", startColorChange);
+  startRef.addEventListener('click', startColorChange);
 }
